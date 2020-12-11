d29bb3ab82ba3d3aba122fa65c98127e2d088b2f
asa-Q <asa.qu@qq.com>
2020-12-11 08:19:09 +0000

    test infura

---
+++ defi/scripts/testInfur.sh
@@ -0,0 +1,2 @@
+curl https://mainnet.infura.io/v3/81761ff4740b4706ad7cf1f21c9fde57 -X POST --data '{"jsonrpc":"2.0","method":"eth_call","params":[{"to":"0xa4e8c3ec456107ea67d3075bf9e3df3a75823db0", "data":"0x70a082310000000000000000000000005f726a5cfcfa578963d2cb319c875968a6232419"}, "latest"],"id":1 }'
+
